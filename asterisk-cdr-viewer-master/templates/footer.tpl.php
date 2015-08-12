1f201f6da540edba6b604ababe2bf0d83d56100e
vishalzala <vishal_zala@yahoo.com>
2015-08-12 23:40:59 +1000

    new fiesl added

---
+++ asterisk-cdr-viewer-master/templates/footer.tpl.php
@@ -0,0 +1,8 @@
+<div id="footer">
+<p>
+	<a href="https://github.com/g613/asterisk-cdr-viewer">Asterisk CDR viewer ( v.1.0.8 )</a>
+    <!-- by Igor Okunev ( igor.okunev [at] gmail.com ) -->
+</p>
+</div>
+</body>
+</html>
